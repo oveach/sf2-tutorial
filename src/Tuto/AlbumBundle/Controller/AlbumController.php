8d7c3b59ca037b41969bbbae590294d5600fedb5
ovh <oveach@gmail.com>
2012-10-29 22:37:04 +0100

    Add new album with basic form

--- src/Tuto/AlbumBundle/Controller/AlbumController.php
+++ src/Tuto/AlbumBundle/Controller/AlbumController.php
@@ -3,7 +3,9 @@
 namespace Tuto\AlbumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Tuto\AlbumBundle\Entity\Album;
+use Tuto\AlbumBundle\Form\AlbumType;
 
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -20,5 +22,41 @@ class AlbumController extends Controller
         $em = $this->getDoctrine()->getEntityManager();
         $albums = $em->getRepository('TutoAlbumBundle:Album')->findAll();
         return array('albums' => $albums);
+    }
+    
+    /**
+     * @Route("/edit", name="edit")
+     * @Template()
+     */
+    public function editAction(Request $request)
+    {
+        $em = $this->get('doctrine')->getEntityManager();
+//         if (!empty($request->get("id"))) {
+//             $album = $em->find('TutoAlbumBundle:Album', $request->get("id"));
+//             $submitValue = "Edit";
+//             $title = "Edit album";
+//         } else {
+            $album = new Album();
+            $submitValue = "Add";
+            $title = "Add new album";
+//         }
+        
+        $form = $this->createForm(new AlbumType(), $album);
+        
+        if ($request->getMethod() == 'POST') {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $em->persist($album);
+                $em->flush();
+                $this->get('session')->getFlashBag()->add('notice', "Album saved successfully");
+                return $this->redirect($this->generateUrl("_home"));
+            }
+        }
+        
+        return array(
+            'form' => $form->createView(),
+            'submitValue' => $submitValue,
+            'title' => $title,
+        );
     }
 }
---
+++ src/Tuto/AlbumBundle/Form/AlbumType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tuto\AlbumBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class AlbumType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title', 'text', array('label' => "Title"));
+        $builder->add('artist', 'text', array('label' => "Artist"));
+        $builder->add('id', 'hidden');
+    }
+
+    public function getName()
+    {
+        return 'album';
+    }
+}
