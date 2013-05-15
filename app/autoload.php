9268e662056deee66c5e242ebe17ead548bf2b62
ovh <oveach@gmail.com>
2013-05-15 23:01:05 +0200

    Reinstall dms filter lib manually

--- app/autoload.php
+++ app/autoload.php
@@ -4,6 +4,10 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+// load additionnal libs manually
+$loader->add('DMS\Filter', __DIR__ . '/../vendor/dms/dms-filter');
+$loader->add('DMS\Bundle\FilterBundle', __DIR__ . '/../vendor/dms/dms-filter-bundle');
+
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
